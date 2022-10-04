45755847f846e4200c85afb9e6836477eabef3b4
Dmytro K <dimakopyl22@gmail.com>
2022-10-04 18:46:17 +0300

    Add Screens
    Manidest
    Theme
    Splash
    Logo

---
+++ Taxi Home/app/src/main/java/com/ua/taxihome/screens/SplashScreenActivity.kt
@@ -0,0 +1,24 @@
+package com.ua.taxihome.screens
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.ua.taxihome.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+            }, 1750)
+    }
+}
